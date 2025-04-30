9ad4cc5e7e700bdf3d4ab474a80572276fea1238
vvyre <seung.yoon.yu@gmail.com>
2025-04-30 19:55:14 +0900

    fix deploy config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 
 export default defineConfig({
-  base: '/<REPO_NAME>/',
   plugins: [react(), tsconfigPaths(), vanillaExtractPlugin()],
   build: {
     outDir: 'docs',
