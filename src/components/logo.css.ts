8762d6a686d52bfe35a9b3beda74789e6f3275b8
vvyre <seung.yoon.yu@gmail.com>
2025-05-03 16:49:01 +0900

    enlarge logo img

--- src/components/logo.css.ts
+++ src/components/logo.css.ts
@@ -2,6 +2,6 @@ import { style } from '@vanilla-extract/css';
 
 export const LOGO_IMG = style({
   borderRadius: '0.75rem',
-  width: '3rem',
-  height: '3rem',
+  width: '4rem',
+  height: '4rem',
 });
