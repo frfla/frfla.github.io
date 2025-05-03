90ce53e82dd7c9d05ac005f1847efeae0ab88137
vvyre <seung.yoon.yu@gmail.com>
2025-05-03 16:45:08 +0900

    add footer

---
+++ src/components/sections/footer.css.ts
@@ -0,0 +1,7 @@
+import { style } from '@vanilla-extract/css';
+
+export const FOOTER = style({
+  width: '100%',
+  marginTop: '10rem',
+  textAlign: 'center',
+});
---
+++ src/components/sections/footer.tsx
@@ -0,0 +1,9 @@
+import { FOOTER } from './footer.css';
+
+export function Footer() {
+  return (
+    <footer className={FOOTER}>
+      <span>Copyright 2025. Seungyoon Yu All rights reserved.</span>
+    </footer>
+  );
+}
