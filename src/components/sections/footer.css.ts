0328c7041e40410fb4d37df203f8ef57f644763e
vvyre <seung.yoon.yu@gmail.com>
2025-05-03 17:06:26 +0900

    add footer to layout

--- src/components/sections/footer.css.ts
+++ src/components/sections/footer.css.ts
@@ -4,4 +4,5 @@ export const FOOTER = style({
   width: '100%',
   marginTop: '10rem',
   textAlign: 'center',
+  fontSize: '0.95rem',
 });
--- src/pages/root-layout.tsx
+++ src/pages/root-layout.tsx
@@ -2,12 +2,14 @@ import 'styles/global-style.css';
 import { Outlet } from 'react-router';
 import { Grid } from 'components/_common/grid';
 import { Nav } from 'components/sections/nav';
+import { Footer } from 'components/sections/footer';
 
 export default function RootLayout() {
   return (
     <Grid>
       <Nav />
       <Outlet />
+      <Footer />
     </Grid>
   );
 }
