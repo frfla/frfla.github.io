c9a05abfc3e0adf7043269d82b0b878ab4c2f71a
vvyre <seung.yoon.yu@gmail.com>
2025-05-06 13:44:05 +0900

    expose korean docs basically

--- src/store/constants/meta.ts
+++ src/store/constants/meta.ts
@@ -7,9 +7,9 @@ export const REPO_URL = {
   'react-md-renderer': 'https://raw.githubusercontent.com/frfla/react-md-renderer/refs/heads/main',
 } as const;
 export const CAPTIONS = {
-  'react-md-renderer': 'Easy Markdown Renderer with CSS-Only Customs',
-  'react-hooks': 'Utility Hooks',
-  'react-animated': 'Animatied Hooks and Components: Under Construction',
-  laser: 'JS/TS Utilities',
+  'react-md-renderer': 'CSS로 쉽게 커스텀하는 마크다운 렌더러',
+  'react-hooks': 'React 유틸리티 훅 패키지',
+  'react-animated': '애니메이션 훅과 컴포넌트 (배포 예정)',
+  laser: '자바스크립트/타입스크립트 유틸리티',
   docs: '',
 };
